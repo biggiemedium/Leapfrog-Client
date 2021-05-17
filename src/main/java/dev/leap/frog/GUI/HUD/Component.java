6c3d7802ca386cc36f143748fd765d4eae4681a6
PalitXD <73961134+PalitXD@users.noreply.github.com>
2021-05-17 11:20:06 +0100

    probably broken af but i cant see errors cause broken laptop

---
+++ src/main/java/dev/leap/frog/GUI/HUD/Component.java
@@ -0,0 +1,27 @@
+package dev.leap.frog.GUI.HUD;
+
+private int x;
+private int y;
+
+private String name;
+
+
+public class Component(){
+
+    public Component(int X, int Y, String Name){
+        this.x = X;
+        this.y = Y;
+        this.name = Name;
+    }
+
+    public draw(mouseX, mouseY){
+            this.draw(mouseX, mouseY);
+        }
+
+
+
+
+
+
+
+        }
