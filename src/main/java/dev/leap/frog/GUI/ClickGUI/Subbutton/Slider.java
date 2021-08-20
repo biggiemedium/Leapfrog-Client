de5a813bc8d466ab979655fd396c259937098238
Concorde69 <73961134+Concorde69@users.noreply.github.com>
2021-08-21 00:48:16 +0100

    BJ

--- src/main/java/dev/leap/frog/GUI/ClickGUI/Subbutton/Slider.java
+++ src/main/java/dev/leap/frog/GUI/ClickGUI/Subbutton/Slider.java
@@ -7,6 +7,6 @@ import java.awt.*;
 public class Slider {
     public static void Slider(int x, int y, int width, int height, int val){
         Gui.drawRect(x + width, y, x + width, y + height, Color.green.getRGB());
-        System.out.println("e");
+
     }
 }
